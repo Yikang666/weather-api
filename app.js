39e13d11aab1f0f3cd264d17c849e4d054d1f075
李羿慷 <2749436915@qq.com>
2024-07-11 13:08:27 +0800

    update: 首次提交

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const axios = require("axios");
+const zlib = require("zlib");
+const app = express();
+const port = 3000;
+
+app.all("*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send({ msg: "天气API服务正常" });
+});
+
+app.get("/api/*", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://i5.weather.oppomobile.com/${req.params[0]}`,
+      {
+        responseType: "arraybuffer",
+      }
+    );
+
+    const buffer = Buffer.from(response.data, "binary");
+    zlib.gunzip(buffer, (err, decompressedData) => {
+      if (err) {
+        return res.status(500).send({ msg: "数据解压失败" });
+      }
+      res.json(JSON.parse(decompressedData.toString()));
+    });
+  } catch (error) {
+    res.status(500).send({ msg: "数据获取失败" });
+  }
+});
+
+app.get("/location/*", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://location.apps.oppomobile.com/xlocations/v1/${req.params[0]}`,
+      {
+        responseType: "arraybuffer",
+      }
+    );
+
+    const buffer = Buffer.from(response.data, "binary");
+    zlib.gunzip(buffer, (err, decompressedData) => {
+      if (err) {
+        return res.status(500).send({ msg: "数据解压失败" });
+      }
+      res.json(JSON.parse(decompressedData.toString()));
+    });
+  } catch (error) {
+    res.status(500).send({ msg: "数据获取失败" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
